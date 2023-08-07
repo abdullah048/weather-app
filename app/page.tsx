a19bbd762e9b81f445d65890174a633f9e394548
abdullah048 <abdullahshahbaz048@gmail.com>
2023-08-07 18:37:34 +0500

    city & country picker done

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <main className='min-h-screen bg-gradient-to-br from-[#394f68] to-[#183b7e] p-10 flex flex-col justify-center'>
       <Card className='text-center max-w-5xl mx-auto'>
-        <Text className='text-6xl font-bold mb-10'>Weather App</Text>
+        <Text className='md:!text-6xl !text-4xl font-bold mb-10'>
+          Weather App
+        </Text>
         <Subtitle className='text-xl'>
           Powered by Next 13.3, tailwind CSS, Tremor 2.0
         </Subtitle>
--- components/CItyPicker.tsx
+++ components/CItyPicker.tsx
@@ -1,10 +1,34 @@
 'use client';
-import { Country } from 'country-state-city';
+import { Country, City } from 'country-state-city';
+import { useState } from 'react';
 import Select from 'react-select';
+import { useRouter } from 'next/navigation';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+
+type CountryOption = {
+  value: {
+    latitude: string;
+    longitude: string;
+    isoCode: string;
+  };
+  label: string;
+} | null;
+
+type CityOption = {
+  value: {
+    latitude: string;
+    longitude: string;
+    countryCode: string;
+    name: string;
+    stateCode: string;
+  };
+  label: string;
+} | null;
+
 type Props = {};
 
 // HACK: transform all countries data to custom array so we can use it in react-select...
-const options = Country.getAllCountries().map(country => ({
+const countriesOptions = Country.getAllCountries().map(country => ({
   value: {
     latitude: country.latitude,
     longitude: country.longitude,
@@ -14,9 +38,59 @@ const options = Country.getAllCountries().map(country => ({
 }));
 
 const CityPicker = (props: Props) => {
+  const [selectedCountry, setSelectedCountry] = useState<CountryOption>(null);
+  const [selectedCity, setSelectedCity] = useState<CityOption>(null);
+  const router = useRouter();
+
+  function handleCountryChange(option: CountryOption) {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  }
+
+  function handleCityChange(option: CityOption) {
+    setSelectedCity(option);
+    router.push(
+      `/location/${option?.value.latitude}/${option?.value.longitude}`
+    );
+  }
+
   return (
-    <div>
-      <Select options={options} />
+    <div className='space-y-4'>
+      <div className='space-y-2'>
+        <div className='flex space-x-2 items-center text-white/80'>
+          <GlobeAltIcon className='h-5 w-5 text-white' />
+          <label htmlFor='country'>Country</label>
+        </div>
+        <Select
+          value={selectedCountry}
+          options={countriesOptions}
+          onChange={handleCountryChange}
+        />
+      </div>
+      {selectedCountry && (
+        <div className='space-y-2'>
+          <div className='flex space-x-2 items-center text-white/80'>
+            <GlobeAltIcon className='h-5 w-5 text-white' />
+            <label htmlFor='country'>City</label>
+          </div>
+          <Select
+            value={selectedCity}
+            options={City.getCitiesOfCountry(
+              selectedCountry.value.isoCode
+            )?.map(city => ({
+              value: {
+                latitude: city.latitude ?? '',
+                longitude: city.longitude ?? '',
+                countryCode: city.countryCode,
+                stateCode: city.stateCode,
+                name: city.name,
+              },
+              label: city.name,
+            }))}
+            onChange={handleCityChange}
+          />
+        </div>
+      )}
     </div>
   );
 };
